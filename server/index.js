33578e3c1c4d948ee35a31f55651a815193782df
jam-jam200 <jessieoyaks4j@gmail.com>
2022-07-07 03:35:46 +0100

    email

--- server/index.js
+++ server/index.js
@@ -4,15 +4,43 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "../config.env" });
 const AppError = require("./utils/appError");
 const app = express();
-require('dotenv').config();
-const nodemailer = require('nodemailer');
-
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use("/", express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.post("/contact-us", (req, res) => {
+  console.log(req.body);
 
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "jessieoyaks4j@gmail.com",
+      password: "jessica4j2852003.",
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: "jessieoyaks4j@gmail.com",
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    // telephone: req.body.phone,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("success")
+    }
+  });
+});
 const homeRoute = require("./routes/app.routes");
 app.use("/", homeRoute);
 
